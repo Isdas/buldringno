0c220c1695a6a939edf736ed268048ac624aa8b8
Erik Aasmundrud <erik_aasmundrud@hotmail.com>
2016-08-28 15:28:56 +0200

    Initial commit

---
+++ src/PhotoGallery/wwwroot/app/app.ts
@@ -0,0 +1,53 @@
+﻿/// <reference path="../../typings/globals/es6-shim/index.d.ts" />
+
+import {provide, Component, OnInit } from '@angular/core';
+import {CORE_DIRECTIVES} from '@angular/common';
+import { RouteConfig, ROUTER_DIRECTIVES } from '@angular/router-deprecated';
+import { Location } from '@angular/common';
+import 'rxjs/add/operator/map';
+import {enableProdMode} from '@angular/core';
+
+enableProdMode();
+import { Routes, APP_ROUTES } from './routes';
+import { MembershipService } from './core/services/membershipService';
+import { User } from './core/domain/user';
+
+@Component({
+    selector: 'photogallery-app',
+    templateUrl: './app/app.html',
+    directives: [ROUTER_DIRECTIVES, CORE_DIRECTIVES]
+})
+@RouteConfig(APP_ROUTES)
+export class AppRoot implements OnInit {
+    private routes = Routes;
+
+    constructor(public membershipService: MembershipService,
+                public location: Location) { }
+
+    ngOnInit() {
+        this.routes = Routes;
+        this.location.go('/');
+    }
+
+    isUserLoggedIn(): boolean {
+        return this.membershipService.isUserAuthenticated();
+    }
+
+    getUserName(): string {
+        if (this.isUserLoggedIn()) {
+            var _user = this.membershipService.getLoggedInUser();
+            return _user.Username;
+        }
+        else
+            return 'Account';
+    }
+
+    logout(): void {
+        this.membershipService.logout()
+            .subscribe(res => {
+                localStorage.removeItem('user');
+            },
+            error => console.error('Error: ' + error),
+            () => { });
+    }
+}
