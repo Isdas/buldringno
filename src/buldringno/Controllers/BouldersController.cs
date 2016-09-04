cdac6c804d94320ca90c142303337bfd97950bd3
Erik Aasmundrud <erik_aasmundrud@hotmail.com>
2016-09-04 21:35:39 +0200

    Cleanup BouldersController

--- src/buldringno/Controllers/BouldersController.cs
+++ src/buldringno/Controllers/BouldersController.cs
@@ -3,12 +3,10 @@ using BuldringNo.Entities;
 using BuldringNo.Infrastructure.Core;
 using BuldringNo.Infrastructure.Repositories;
 using BuldringNo.ViewModels;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,58 +15,47 @@ namespace BuldringNo.Controllers
     [Route("api/[controller]")]
     public class BouldersController : Controller
     {
-        private readonly IAuthorizationService _authorizationService;
         private readonly IBoulderRepository _boulderRepository;
         private readonly ILoggingRepository _loggingRepository;
 
-        public BouldersController(IAuthorizationService authorizationService,
-                                IBoulderRepository boulderRepository,
-                                ILoggingRepository loggingRepository)
+        public BouldersController(IBoulderRepository boulderRepository,
+                                  ILoggingRepository loggingRepository)
         {
-            _authorizationService = authorizationService;
             _boulderRepository = boulderRepository;
             _loggingRepository = loggingRepository;
         }
 
         [HttpGet("{page:int=0}/{pageSize=12}")]
-        public async Task<IActionResult> Get(int? page, int? pageSize)
+        public PaginationSet<BoulderViewModel> Get(int? page, int? pageSize)
         {
             PaginationSet<BoulderViewModel> pagedSet = new PaginationSet<BoulderViewModel>();
 
             try
             {
-                if (await _authorizationService.AuthorizeAsync(User, "AdminOnly"))
+                int currentPage = page.Value;
+                int currentPageSize = pageSize.Value;
+
+                List<Boulder> _boulders = null;
+                int _totalBoulders = new int();
+
+                _boulders = _boulderRepository
+                    .AllIncluding(a => a.Problems)
+                    .OrderBy(a => a.Id)
+                    .Skip(currentPage * currentPageSize)
+                    .Take(currentPageSize)
+                    .ToList();
+
+                _totalBoulders = _boulderRepository.GetAll().Count();
+
+                IEnumerable<BoulderViewModel> _bouldersVM = Mapper.Map<IEnumerable<Boulder>, IEnumerable<BoulderViewModel>>(_boulders);
+
+                pagedSet = new PaginationSet<BoulderViewModel>()
                 {
-                    int currentPage = page.Value;
-                    int currentPageSize = pageSize.Value;
-
-                    List<Boulder> _boulders = null;
-                    int _totalBoulders = new int();
-
-                    _boulders = _boulderRepository
-                        .AllIncluding(a => a.Problems)
-                        .OrderBy(a => a.Id)
-                        .Skip(currentPage * currentPageSize)
-                        .Take(currentPageSize)
-                        .ToList();
-
-                    _totalBoulders = _boulderRepository.GetAll().Count();
-
-                    IEnumerable<BoulderViewModel> _bouldersVM = Mapper.Map<IEnumerable<Boulder>, IEnumerable<BoulderViewModel>>(_boulders);
-
-                    pagedSet = new PaginationSet<BoulderViewModel>()
-                    {
-                        Page = currentPage,
-                        TotalCount = _totalBoulders,
-                        TotalPages = (int)Math.Ceiling((decimal)_totalBoulders / currentPageSize),
-                        Items = _bouldersVM
-                    };
-                }
-                else
-                {
-                    CodeResultStatus _codeResult = new CodeResultStatus(401);
-                    return new ObjectResult(_codeResult);
-                }
+                    Page = currentPage,
+                    TotalCount = _totalBoulders,
+                    TotalPages = (int)Math.Ceiling((decimal)_totalBoulders / currentPageSize),
+                    Items = _bouldersVM
+                };
             }
             catch (Exception ex)
             {
@@ -76,7 +63,7 @@ namespace BuldringNo.Controllers
                 _loggingRepository.Commit();
             }
 
-            return new ObjectResult(pagedSet);
+            return pagedSet;
         }
 
         [HttpGet("{id:int}/problems/{page:int=0}/{pageSize=12}")]
