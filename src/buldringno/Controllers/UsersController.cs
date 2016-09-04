a91a2687ee5fac633d8d4c79d4c1f201d2992cc9
Erik Aasmundrud <erik_aasmundrud@hotmail.com>
2016-09-04 21:36:20 +0200

    Added (Policy = "AdminOnly") for the users

--- src/buldringno/Controllers/UsersController.cs
+++ src/buldringno/Controllers/UsersController.cs
@@ -3,10 +3,12 @@ using BuldringNo.Entities;
 using BuldringNo.Infrastructure.Core;
 using BuldringNo.Infrastructure.Repositories;
 using BuldringNo.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -15,46 +17,59 @@ namespace BuldringNo.Controllers
     [Route("api/[controller]")]
     public class UsersController : Controller
     {
+        private readonly IAuthorizationService _authorizationService;
         private readonly IUserRepository _userRepository;
         private readonly ILoggingRepository _loggingRepository;
 
-        public UsersController(IUserRepository userRepository, ILoggingRepository loggingRepository)
+        public UsersController(IAuthorizationService authorizationService,
+                               IUserRepository userRepository,
+                               ILoggingRepository loggingRepository)
         {
+            _authorizationService = authorizationService;
             _userRepository = userRepository;
             _loggingRepository = loggingRepository;
         }
 
+        [Authorize(Policy = "AdminOnly")]
         [HttpGet("{page:int=0}/{pageSize=12}")]
-        public PaginationSet<UserViewModel> Get(int? page, int? pageSize)
+        public async Task<IActionResult> Get(int? page, int? pageSize)
         {
             PaginationSet<UserViewModel> pagedSet = null;
 
             try
             {
-                int currentPage = page.Value;
-                int currentPageSize = pageSize.Value;
-
-                List<User> _users = null;
-                int _totalUsers = new int();
-
-                _users = _userRepository
-                    .GetAll()
-                    .OrderBy(p => p.Id)
-                    .Skip(currentPage * currentPageSize)
-                    .Take(currentPageSize)
-                    .ToList();
-
-                _totalUsers = _userRepository.GetAll().Count();
-
-                IEnumerable<UserViewModel> _usersVM = Mapper.Map<IEnumerable<User>, IEnumerable<UserViewModel>>(_users);
-
-                pagedSet = new PaginationSet<UserViewModel>()
+                if (await _authorizationService.AuthorizeAsync(User, "AdminOnly"))
                 {
-                    Page = currentPage,
-                    TotalCount = _totalUsers,
-                    TotalPages = (int)Math.Ceiling((decimal)_totalUsers / currentPageSize),
-                    Items = _usersVM
-                };
+                    int currentPage = page.Value;
+                    int currentPageSize = pageSize.Value;
+
+                    List<User> _users = null;
+                    int _totalUsers = new int();
+
+                    _users = _userRepository
+                        .GetAll()
+                        .OrderBy(p => p.Id)
+                        .Skip(currentPage * currentPageSize)
+                        .Take(currentPageSize)
+                        .ToList();
+
+                    _totalUsers = _userRepository.GetAll().Count();
+
+                    IEnumerable<UserViewModel> _usersVM = Mapper.Map<IEnumerable<User>, IEnumerable<UserViewModel>>(_users);
+
+                    pagedSet = new PaginationSet<UserViewModel>()
+                    {
+                        Page = currentPage,
+                        TotalCount = _totalUsers,
+                        TotalPages = (int)Math.Ceiling((decimal)_totalUsers / currentPageSize),
+                        Items = _usersVM
+                    };
+                }
+                else
+                {
+                    CodeResultStatus _codeResult = new CodeResultStatus(401);
+                    return new ObjectResult(_codeResult);
+                }
             }
             catch (Exception ex)
             {
@@ -62,9 +77,10 @@ namespace BuldringNo.Controllers
                 _loggingRepository.Commit();
             }
 
-            return pagedSet;
+            return new ObjectResult(pagedSet);
         }
 
+        [Authorize(Policy = "AdminOnly")]
         [HttpDelete("{id:int}")]
         public IActionResult Delete(int id)
         {
