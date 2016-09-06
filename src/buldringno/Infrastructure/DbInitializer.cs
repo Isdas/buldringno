8436a5fb56567447c4fbde53d8344e1fed1f939e
Erik Aasmundrud <erik_aasmundrud@hotmail.com>
2016-09-06 20:58:23 +0200

    Added real testdata

--- src/buldringno/Infrastructure/DbInitializer.cs
+++ src/buldringno/Infrastructure/DbInitializer.cs
@@ -1,5 +1,4 @@
 ﻿using BuldringNo.Entities;
-using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -23,57 +22,377 @@ namespace BuldringNo.Infrastructure
             if (!context.Boulders.Any())
             {
                 List<Boulder> _boulders = new List<Boulder>();
+                string[] _images = Directory.GetFiles(Path.Combine(imagesPath, "images"));
 
                 var _boulder1 = context.Boulders.Add(
                     new Boulder
                     {
                         DateCreated = DateTime.Now,
-                        Title = "Bulder 1",
-                        Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                        Title = "Barnehageveggen",
+                        Description = "Veggen rett ved barnehagen."
                     }).Entity;
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "Kruse",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[0]),
+                         Boulder = _boulder1,
+                         Grade = "5B"
+                     });
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "Kulturell elite",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[0]),
+                         Boulder = _boulder1,
+                         Grade = "6B+"
+                     });
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "Kulturell elite - Definert start",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[0]),
+                         Boulder = _boulder1,
+                         Grade = "7A"
+                     });
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "White trash",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[0]),
+                         Boulder = _boulder1,
+                         Grade = "6A"
+                     });
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "White trash - Definert start",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[0]),
+                         Boulder = _boulder1,
+                         Grade = "7A"
+                     });
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "White trash II",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[0]),
+                         Boulder = _boulder1,
+                         Grade = "6B"
+                     });
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "Mens vi venter på Godoffe",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[0]),
+                         Boulder = _boulder1,
+                         Grade = "7B+"
+                     });
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "Kardemomme by (SS)",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[0]),
+                         Boulder = _boulder1,
+                         Grade = "7B+"
+                     });
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "Herkules",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[0]),
+                         Boulder = _boulder1,
+                         Grade = "7A+"
+                     });
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "David",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[0]),
+                         Boulder = _boulder1,
+                         Grade = "6A"
+                     });
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "David (SS)",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[0]),
+                         Boulder = _boulder1,
+                         Grade = "6C+"
+                     });
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "Påskråriss",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[0]),
+                         Boulder = _boulder1,
+                         Grade = "6C"
+                     });
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "Saft suse",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[0]),
+                         Boulder = _boulder1,
+                         Grade = "5A"
+                     });
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "Markjordbær",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[0]),
+                         Boulder = _boulder1,
+                         Grade = "6B+"
+                     });
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "Blåbær",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[0]),
+                         Boulder = _boulder1,
+                         Grade = "3B"
+                     });
+
                 var _boulder2 = context.Boulders.Add(
                     new Boulder
                     {
                         DateCreated = DateTime.Now,
-                        Title = "Bulder 2",
-                        Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                        Title = "Dallsteinen",
+                        Description = "Liten slopete stein."
                     }).Entity;
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "Indisk dall (SS)",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[1]),
+                         Boulder = _boulder2,
+                         Grade = "7A"
+                     });
+
                 var _boulder3 = context.Boulders.Add(
                     new Boulder
                     {
                         DateCreated = DateTime.Now,
-                        Title = "Bulder 3",
-                        Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                        Title = "Hullet",
+                        Description = "Lang stein med god landing."
                     }).Entity;
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "Hullsag",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[2]),
+                         Boulder = _boulder3,
+                         Grade = "3B"
+                     });
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "Hullsag (SS)",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[2]),
+                         Boulder = _boulder3,
+                         Grade = "5C"
+                     });
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "Risset",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[2]),
+                         Boulder = _boulder3,
+                         Grade = "6B+"
+                     });
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "Hullet",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[2]),
+                         Boulder = _boulder3,
+                         Grade = "6C+"
+                     });
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "Hullet (SS)",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[2]),
+                         Boulder = _boulder3,
+                         Grade = "7B"
+                     });
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "Bråthen safe",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[2]),
+                         Boulder = _boulder3,
+                         Grade = "7B"
+                     });
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "Rednecks",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[2]),
+                         Boulder = _boulder3,
+                         Grade = "7A"
+                     });
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "Sofa slækking",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[2]),
+                         Boulder = _boulder3,
+                         Grade = "7A"
+                     });
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "Tja",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[2]),
+                         Boulder = _boulder3,
+                         Grade = "5C"
+                     });
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "Høyre prassel",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[2]),
+                         Boulder = _boulder3,
+                         Grade = "3B"
+                     });
+
                 var _boulder4 = context.Boulders.Add(
                     new Boulder
                     {
                         DateCreated = DateTime.Now,
-                        Title = "Bulder 4",
-                        Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                        Title = "Islas Canarias",
+                        Description = "Stein litt inn fra stien."
                     }).Entity;
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "Islas Canarias",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[3]),
+                         Boulder = _boulder4,
+                         Grade = "7A+"
+                     });
 
-                _boulders.Add(_boulder1); _boulders.Add(_boulder2); _boulders.Add(_boulder3); _boulders.Add(_boulder4);
+                var _boulder5 = context.Boulders.Add(
+                    new Boulder
+                    {
+                        DateCreated = DateTime.Now,
+                        Title = "Pølse i lompe",
+                        Description = "Stein rett ved et mygghøl."
+                    }).Entity;
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "Death Star canteen",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[4]),
+                         Boulder = _boulder5,
+                         Grade = "5A"
+                     });
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "OL i mantling",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[4]),
+                         Boulder = _boulder5,
+                         Grade = "7A"
+                     });
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "Jernbanen",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[4]),
+                         Boulder = _boulder5,
+                         Grade = "3C"
+                     });
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "Country Man",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[4]),
+                         Boulder = _boulder5,
+                         Grade = "5B"
+                     });
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "Lompe",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[4]),
+                         Boulder = _boulder5,
+                         Grade = "6A"
+                     });
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "Lompe (SS)",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[4]),
+                         Boulder = _boulder5,
+                         Grade = "7A+"
+                     });
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "Pølse i lompe",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[4]),
+                         Boulder = _boulder5,
+                         Grade = "6C+"
+                     });
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "Pølse i lompe (SS)",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[4]),
+                         Boulder = _boulder5,
+                         Grade = "7B"
+                     });
+                context.Problems.Add(
+                     new Problem()
+                     {
+                         Title = "Kebab",
+                         DateUploaded = DateTime.Now,
+                         Uri = Path.GetFileName(_images[4]),
+                         Boulder = _boulder5,
+                         Grade = "3B"
+                     });
 
-                string[] _images = Directory.GetFiles(Path.Combine(imagesPath, "images"));
-                Random rnd = new Random();
-
-                foreach (string _image in _images)
-                {
-                    int _selectedBoulder = rnd.Next(1, 4);
-                    string _fileName = Path.GetFileName(_image);
-
-                    context.Problems.Add(
-                        new Problem()
-                        {
-                            Title = _fileName,
-                            DateUploaded = DateTime.Now,
-                            Uri = _fileName,
-                            Boulder = _boulders.ElementAt(_selectedBoulder),
-                            Grade = "7A"
-                        }
-                        );
-                }
+                _boulders.Add(_boulder1);
+                _boulders.Add(_boulder2);
+                _boulders.Add(_boulder3);
+                _boulders.Add(_boulder4);
+                _boulders.Add(_boulder5);
 
                 context.SaveChanges();
             }
