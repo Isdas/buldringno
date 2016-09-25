b97f3dfc3bf237f0aef54c6d88bd459118b3e3c1
Erik Aasmundrud <erik_aasmundrud@hotmail.com>
2016-09-25 19:31:23 +0200

    Improved "areas"-view page

--- src/buldringno/wwwroot/app/core/domain/area.ts
+++ src/buldringno/wwwroot/app/core/domain/area.ts
@@ -1,18 +1,27 @@
 ﻿export class Area {
     Id: number;
     Title: string;
-    Description: string;
+    DescriptionMain: string;
+    DescriptionSecondary: string;
+    Parking: string;
+    ApproachTime: string;
     DateCreated: Date;
     TotalBoulders: number;
 
     constructor(id: number,
         title: string,
-        description: string,
+        descriptionMain: string,
+        descriptionSecondary: string,
+        parking: string,
+        approachTime: string,
         dateCreated: Date,
         totalBoulders: number) {
         this.Id = id;
         this.Title = title;
-        this.Description = description;
+        this.DescriptionMain = descriptionMain;
+        this.DescriptionSecondary = descriptionSecondary;
+        this.Parking = parking;
+        this.ApproachTime = approachTime;
         this.DateCreated = dateCreated;
         this.TotalBoulders = totalBoulders;
     }
--- src/buldringno/wwwroot/lib/spa/core/domain/area.js
+++ src/buldringno/wwwroot/lib/spa/core/domain/area.js
@@ -1,9 +1,12 @@
 "use strict";
 var Area = (function () {
-    function Area(id, title, description, dateCreated, totalBoulders) {
+    function Area(id, title, descriptionMain, descriptionSecondary, parking, approachTime, dateCreated, totalBoulders) {
         this.Id = id;
         this.Title = title;
-        this.Description = description;
+        this.DescriptionMain = descriptionMain;
+        this.DescriptionSecondary = descriptionSecondary;
+        this.Parking = parking;
+        this.ApproachTime = approachTime;
         this.DateCreated = dateCreated;
         this.TotalBoulders = totalBoulders;
     }
